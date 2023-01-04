d6847ea973c91b601221b7d9e61d4e27fe1d36cf
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-01-04 20:53:25 +0100

    old deployer back on track

--- deploy.php
+++ deploy.php
@@ -15,29 +15,31 @@ set('shared_files', ['public/wp-config.php', 'public/.htaccess']);
 set('shared_dirs', ['public/wp-content/uploads']);
 set('keep_releases', 3);
 
-// AFTER WP
-
-#set('shared_files', ['public/index.html', 'public/.htaccess']);
-#set('shared_dirs', ['public/wp-content/uploads']);
 
 // Writable dirs by web server
-// set('writable_mode', 'chown');
+set('writable_mode', 'chown');
 // set('writable_dirs', ['public/wp-content/uploads']);
-//set('allow_anonymous_stats', false);
-
-
+set('allow_anonymous_stats', false);
 
 // Hosts
-host('88.198.150.108')
-        ->set('remote_user','getz')
-        //->set('remote_user','aaron')
-        // ->set('become', 'root')
+host('Lillesand')
+        ->set('remote_user','ben')
+      
 
-        ->set('deploy_path', '/var/www/getz-fotografie.com');
+        ->set('deploy_path', '/var/www/backit');
         //->set('deploy_path', '~/app');
 
 // Composer
 set('composer_action', false);
+
+
+
+task('post-deploy', function () {
+    
+    run('sudo chown -R ben:www-data /var/www/backit');
+    run('sudo chmod -R u+rwx /var/www/backit');
+  });
+
 
 // Tasks
 desc('Deploy your project');
@@ -52,10 +54,19 @@ task('deploy', [
     //'deploy:clear_paths',
     //'deploy:symlink',
     //'deploy:unlock',
-     'deploy:publish'
+    'post-deploy',
+    'deploy:publish'
+     
 ]);
 
-
+task('delete_src_folder', function () {
+        run('rm -rf {{release_path}}/src');
+    });
 
 // [Optional] If deploy fails automatically unlock.
+after('deploy', 'delete_src_folder');
+
 after('deploy:failed', 'deploy:unlock');
+
+
+
